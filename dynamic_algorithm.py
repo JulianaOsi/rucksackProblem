a22470789d5dd969b99f18d9ad878df0c9ef0f13
JulianaOsi <julia.osintseva15@gmail.com>
2019-06-09 00:29:33 +0500

    packed

--- dynamic_algorithm.py
+++ dynamic_algorithm.py
@@ -1,31 +1,54 @@
 import pandas as pd
 
-def dynamic(weights, prices, rucksack_weight):
+def dynamic(weights, profits, capacity, size):
+
     table = []
     prev_max = 0
-    for i in range(0, len(prices)):
+    for i in range(0, size):
         table.append([])
-        for j in range(0, rucksack_weight):
+        for j in range(0, capacity):
             another_price = 0
             delta_weight = j + 1 - weights[i]  # оставшийся вес
-
             if i > 0:
                 prev_max = table[i - 1][j]  # предыдущий максимум
                 if delta_weight > 0:
                     another_price = table[i - 1][delta_weight - 1]  # стоимость оставшегося места
-
             if delta_weight < 0:
                 table[i].append(prev_max)
             else:
-                table[i].append(max(prev_max, prices[i] + another_price))
+                table[i].append(max(prev_max, profits[i] + another_price))
     return table
 
+df = pd.read_csv('data24.csv', sep=';')
+#print(df)
+weights = df['weights']
+profits = df['profits']
+size = int(df['size'][:1])
+capacity = int(df['capacity'][:1])
+exp_result = int(df['result'][:1])
 
-df = pd.read_csv('data20.csv', header=0, sep=';')
-print(df)
-prices = df['price']
-weights = df['weight']
-print(prices, weights)
-rucksack_weight = 1278
+#print(size)
+#print(capacity)
+#print(result)
 
-print(dynamic(weights, prices, rucksack_weight))
+knapsack = dynamic(weights, profits, capacity, size)
+print(knapsack)
+
+result = knapsack[size - 1][capacity-1]
+col = capacity-1
+packed = []
+sum = 0
+for row in range(size-1, -1, -1):
+    if row == 0 and knapsack[row][col] != 0:
+        packed.insert(0, row)
+    elif knapsack[row][col] != knapsack[row - 1][col]:
+        packed.insert(0, row)
+        col -= weights[row]
+        sum += profits[row]
+        if sum == result:
+            break
+
+print(packed)
+
+
+
